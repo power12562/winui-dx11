bbd7724f51a9c9deb6de21e6283680ba501b8726
power12562 <kimsiwoo1005@gmail.com>
2026-03-13 16:49:49 +0900

    feature(core): inputsystem cs 추가

---
+++ WsiuEngine/Core/System/InputSystem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WsiuRenderer;
+
+namespace WsiuEngine.Core.System
+{
+    public class InputSystem(EngineCore engineCore)
+    {
+        private EngineCore _engine = engineCore;
+
+        private KeyboardInputState _keyboardState;
+        private MouseInputState _mouseInputState;
+        private KeyboardInputState _keyboardLastState;
+        private MouseInputState _mouseInputLastState;
+
+        internal void BeginFrame()
+        {
+            _mouseInputState = _engine.InputMouseState;
+            _keyboardState = _engine.InputKeyboardState;
+        }
+
+        internal void EndFrame()
+        {
+            _mouseInputLastState = _mouseInputState;
+            _keyboardLastState = _keyboardState;
+        }
+    }
+}
