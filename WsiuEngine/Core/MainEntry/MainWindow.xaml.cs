68f222b3268a8502250e0da6209884ed1fb6a57d
power12562 <kimsiwoo1005@gmail.com>
2026-03-13 16:49:38 +0900

    feature(core): mainentry에서 engine클래스를 분리 및 inputsystem 분리

--- WsiuEngine/Core/MainEntry/MainWindow.xaml.cs
+++ WsiuEngine/Core/MainEntry/MainWindow.xaml.cs
@@ -13,47 +13,21 @@ using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
-
-using WsiuRenderer;
-
-namespace WsiuEngine
+namespace WsiuEngine.Core.MainEntry
 {
-    namespace Core
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
     {
-        namespace MainEntry
+        private WsiuEngine _engine;
+
+        public MainWindow()
         {
-            /// <summary>
-            /// An empty window that can be used on its own or navigated to within a Frame.
-            /// </summary>
-            public sealed partial class MainWindow : Window
-            {
-                public MainWindow()
-                {
-                    InitializeComponent();
-                    InitializeEngineCore();
-                }
-
-                private EngineCore? _engine;
-
-                private void InitializeEngineCore()
-                {
-                    var hwnd = WinRT.Interop.WindowNative.GetWindowHandle(this);
-                    _engine = new EngineCore();
-                    _engine.Initialize((ulong)hwnd, EnginePanel);
-                    CompositionTarget.Rendering += (sender, args) => Update();
-                }
-
-                private void Update()
-                {
-                    if (_engine == null) return;
-
-                    _engine.BeginFrame();
-                    _engine.Tick();
-                    _engine.EndFrame();
-                }
-            }
+            InitializeComponent();
+            var hwnd = WinRT.Interop.WindowNative.GetWindowHandle(this);
+            _engine = new WsiuEngine(hwnd, EnginePanel);
+            CompositionTarget.Rendering += (sender, args) => _engine.Update();
         }
     }
 }
---
+++ WsiuEngine/Core/WsiuEngine.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.UI.Xaml.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WsiuEngine.Core.System;
+using WsiuRenderer;
+
+namespace WsiuEngine.Core
+{
+    public class WsiuEngine
+    {
+        private EngineCore _engine;
+        private InputSystem _inputSystem;
+
+        public WsiuEngine(nint hwnd, SwapChainPanel enginePanel)
+        {     
+            _engine = new EngineCore();
+            _engine.Initialize((ulong)hwnd, enginePanel);     
+            _inputSystem = new InputSystem(_engine);
+        }
+
+        public void Update()
+        {
+            _engine.BeginFrame();
+            _inputSystem.BeginFrame();
+            _engine.Tick();
+            _inputSystem.EndFrame();
+            _engine.EndFrame();
+        }
+    }
+}
