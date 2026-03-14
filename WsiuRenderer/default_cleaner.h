53871319ad6990eb0629bff02089e502e683503f
power12562 <kimsiwoo1005@gmail.com>
2026-03-14 12:42:02 +0900

    feature(core): unique_ptr용 기본 Cleaner 추가

--- WsiuRenderer/default_cleaner.h
+++ WsiuRenderer/default_cleaner.h
@@ -4,9 +4,12 @@ template<typename _value_type>
 struct ComPtrCleaner
 {
     using value_type = _value_type;
-    void operator()(ComPtr<value_type>& value) 
-    { 
-        value.Reset();
-    }
+    void operator()(ComPtr<value_type>& value) { value.Reset(); }
 };
 
+template <typename _value_type> 
+struct UniquePtrCleaner
+{
+    using value_type = _value_type;
+    void operator()(std::unique_ptr<value_type>& value) { value.reset(); }
+};
