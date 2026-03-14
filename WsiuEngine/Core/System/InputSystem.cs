7dcc3abc3d97e52f40e27ad4cad63efc33e44b6c
power12562 <kimsiwoo1005@gmail.com>
2026-03-14 10:16:03 +0900

    feature(core): 키보드, 마우스 상태 체크 기능 추가

--- WsiuEngine/Core/System/InputSystem.cs
+++ WsiuEngine/Core/System/InputSystem.cs
@@ -3,29 +3,98 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.System;
 using WsiuRenderer;
 
 namespace WsiuEngine.Core.System
 {
+    public enum KeyState
+    {
+        None,       
+        Down,       
+        Held,       
+        Up          
+    }
+
     public class InputSystem(EngineCore engineCore)
     {
         private EngineCore _engine = engineCore;
 
-        private KeyboardInputState _keyboardState;
-        private MouseInputState _mouseInputState;
         private KeyboardInputState _keyboardLastState;
+        private KeyboardInputState _keyboardState;
         private MouseInputState _mouseInputLastState;
+        private MouseInputState _mouseInputState;
 
-        internal void BeginFrame()
+        public KeyState GetKeyState(Keyboard.KeyCode keyCode)
         {
+            bool last = IsKeyboardBitSet(ref _keyboardLastState, keyCode);
+            bool current = IsKeyboardBitSet(ref _keyboardState, keyCode);
+            if (current)
+            {
+                return last ? KeyState.Held : KeyState.Down;
+            }
+            return last ? KeyState.Up : KeyState.None;
+        }
+
+        public KeyState GetKeyState(Mouse.KeyCode keyCode)
+        {
+            bool last = IsMouseKeySet(ref _mouseInputLastState, keyCode);
+            bool current = IsMouseKeySet(ref _mouseInputState, keyCode);
+            if (current)
+            {
+                return last ? KeyState.Held : KeyState.Down;
+            }
+            return last ? KeyState.Up : KeyState.None;
+        }
+
+        private void UpdateMouse()
+        {
+            _mouseInputLastState = _mouseInputState;
             _mouseInputState = _engine.InputMouseState;
+        }
+
+        private static bool IsMouseKeySet(ref MouseInputState state, Mouse.KeyCode key)
+        {
+            switch (key)
+            {
+                case Mouse.KeyCode.Left:
+                    return state.IsLeftDown;
+                case Mouse.KeyCode.Right:
+                    return state.IsRightDown;
+                case Mouse.KeyCode.Middle:
+                    return state.IsMiddleDown;
+                case Mouse.KeyCode.X1:
+                    return state.IsX1Down;
+                case Mouse.KeyCode.X2:
+                    return state.IsX2Down;
+                default:
+                    return false;
+            }
+        }
+
+        private void UpdateKeyboard()
+        {
+            _keyboardLastState = _keyboardState;
             _keyboardState = _engine.InputKeyboardState;
         }
 
-        internal void EndFrame()
+        private static unsafe bool IsKeyboardBitSet(ref KeyboardInputState state, Keyboard.KeyCode key)
         {
-            _mouseInputLastState = _mouseInputState;
-            _keyboardLastState = _keyboardState;
+            int vk = (int)key;
+            int index = vk / 32;
+            int bitMask = 1 << (vk % 32);
+
+            fixed (KeyboardInputState* ptr = &state)
+            {
+                uint* raw = (uint*)ptr;
+                return (raw[index] & bitMask) != 0;
+            }
+        }
+
+        internal void Update()
+        {
+            UpdateMouse();
+            UpdateKeyboard();
         }
     }
 }
---
+++ WsiuEngine/Core/System/KeyCode.cs
@@ -0,0 +1,94 @@
+﻿namespace WsiuEngine.Core.System
+{
+    namespace Mouse
+    {
+        public enum KeyCode : int
+        {
+            None = 0,
+            Left = 0x01,
+            Right = 0x02,
+            Middle = 0x03,
+            X1 = 0x04,
+            X2 = 0x05
+        }
+    }
+
+    namespace Keyboard
+    {
+        public enum KeyCode : int
+        {
+            None = 0,
+
+            // 제어키
+            Backspace = 0x08,
+            Tab = 0x09,
+            Clear = 0x0C,
+            Return = 0x0D,
+            Pause = 0x13,
+            Escape = 0x1B,
+            Space = 0x20,
+
+            // 특수키
+            PageUp = 0x21,
+            PageDown = 0x22,
+            End = 0x23,
+            Home = 0x24,
+            LeftArrow = 0x25,
+            UpArrow = 0x26,
+            RightArrow = 0x27,
+            DownArrow = 0x28,
+            Insert = 0x2D,
+            Delete = 0x2E,
+
+            // 숫자키 
+            Alpha0 = 0x30, Alpha1 = 0x31, Alpha2 = 0x32, Alpha3 = 0x33, Alpha4 = 0x34,
+            Alpha5 = 0x35, Alpha6 = 0x36, Alpha7 = 0x37, Alpha8 = 0x38, Alpha9 = 0x39,
+
+            // 알파벳 
+            A = 0x41, B = 0x42, C = 0x43, D = 0x44, E = 0x45, F = 0x46, G = 0x47,
+            H = 0x48, I = 0x49, J = 0x4A, K = 0x4B, L = 0x4C, M = 0x4D, N = 0x4E,
+            O = 0x4F, P = 0x50, Q = 0x51, R = 0x52, S = 0x53, T = 0x54, U = 0x55,
+            V = 0x56, W = 0x57, X = 0x58, Y = 0x59, Z = 0x5A,
+
+            // 윈도우 키
+            LeftWindows = 0x5B,
+            RightWindows = 0x5C,
+
+            // 키패드
+            Keypad0 = 0x60, Keypad1 = 0x61, Keypad2 = 0x62, Keypad3 = 0x63,
+            Keypad4 = 0x64, Keypad5 = 0x65, Keypad6 = 0x66, Keypad7 = 0x67,
+            Keypad8 = 0x68, Keypad9 = 0x69,
+            KeypadMultiply = 0x6A, KeypadPlus = 0x6B, KeypadMinus = 0x6D,
+            KeypadPeriod = 0x6E, KeypadDivide = 0x6F,
+
+            // F키
+            F1 = 0x70, F2 = 0x71, F3 = 0x72, F4 = 0x73, F5 = 0x74, F6 = 0x75,
+            F7 = 0x76, F8 = 0x77, F9 = 0x78, F10 = 0x79, F11 = 0x7A, F12 = 0x7B,
+
+            // 상태키
+            Numlock = 0x90,
+            ScrollLock = 0x91,
+
+            // 조합키 
+            LeftShift = 0xA0,
+            RightShift = 0xA1,
+            LeftControl = 0xA2,
+            RightControl = 0xA3,
+            LeftAlt = 0xA4,
+            RightAlt = 0xA5,
+
+            // 기호 및 기타
+            Semicolon = 0xBA,
+            Plus = 0xBB,
+            Comma = 0xBC,
+            Minus = 0xBD,
+            Period = 0xBE,
+            Slash = 0xBF,
+            BackQuote = 0xC0,
+            LeftBracket = 0xDB,
+            Backslash = 0xDC,
+            RightBracket = 0xDD,
+            Quote = 0xDE
+        }
+    }
+}
