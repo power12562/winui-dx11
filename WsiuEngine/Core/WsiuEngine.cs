525d28ed0758efd02e6be0469a46b1540dfec4c7
power12562 <kimsiwoo1005@gmail.com>
2026-03-14 10:16:32 +0900

    refactor(core): 메인 엔진 컨트롤 클래스 싱글톤 추가

--- WsiuEngine/Core/WsiuEngine.cs
+++ WsiuEngine/Core/WsiuEngine.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.UI.Xaml.Controls;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.System;
 using WsiuEngine.Core.System;
 using WsiuRenderer;
 
@@ -11,23 +13,45 @@ namespace WsiuEngine.Core
 {
     public class WsiuEngine
     {
-        private EngineCore _engine;
-        private InputSystem _inputSystem;
+        private static WsiuEngine Instance { get; set; } = null!;
+        public static InputSystem InputSystem { get; private set; } = null!;
+
+        private readonly EngineCore _engine;
+        private readonly InputSystem _inputSystem;
 
         public WsiuEngine(nint hwnd, SwapChainPanel enginePanel)
         {     
+            if (Instance != null) throw new InvalidOperationException("Engine is already initialized!");
+
+            Instance = this;
+
             _engine = new EngineCore();
             _engine.Initialize((ulong)hwnd, enginePanel);     
+
             _inputSystem = new InputSystem(_engine);
+            InputSystem = _inputSystem;
         }
 
         public void Update()
         {
             _engine.BeginFrame();
-            _inputSystem.BeginFrame();
+            _inputSystem.Update();
             _engine.Tick();
-            _inputSystem.EndFrame();
+            Test();
             _engine.EndFrame();
+        }
+
+        private static void Test()
+        {
+            if(InputSystem.GetKeyState(MouseKey.X1) == KeyState.Up)
+            {
+                Debug.WriteLine("mouse key up");
+            }
+
+            if (InputSystem.GetKeyState(VirtualKey.Enter) == KeyState.Up)
+            {
+                Debug.WriteLine("enter key up");
+            }
         }
     }
 }
