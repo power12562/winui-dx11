d5323880ea95336ac11524a89d02550545fb04d8
power12562 <kimsiwoo1005@gmail.com>
2026-03-15 12:07:56 +0900

    feature(core): imguiContext 기능 추가
    imgui 드로우 요청을 쉽게 하기 위한 클래스

---
+++ WsiuRenderer/ImguiContext.cpp
@@ -0,0 +1,49 @@
+﻿#include "pch.h"
+#include "ImguiContext.h"
+#if __has_include("ImguiContext.g.cpp")
+#include "ImguiContext.g.cpp"
+#endif
+#include "imguicommons.h"
+#include "EngineCore.h"
+
+namespace winrt::WsiuRenderer::implementation
+{
+    ImguiContext::ImguiContext(EngineCore const& engineCore) 
+        : 
+        _engineCore(engineCore)
+    {}
+
+    ImguiContext::~ImguiContext() 
+    { 
+        _engineCore.EditorWindowDestroy(_windowID); }
+
+    void ImguiContext::InitializeWindow(hstring const& title) 
+    {
+        _windowID = _engineCore.EditorWindowCreate(title);
+        _engineCore.EditorWindowDrawCallback(_windowID, [this] { DrawCommands(); });
+    }
+
+    void ImguiContext::DrawCommands() 
+    {
+        for (auto& func : _beginCommands)
+        {
+            func();
+        }
+        for (auto& func : _endCommands)
+        {
+            func();
+        }
+    }
+
+    void ImguiContext::SetTitle(hstring const& title) const 
+    { 
+        _engineCore.EditorWindowChangeTitle(_windowID, title);
+    }
+
+    void ImguiContext::Text(hstring const& text)
+    {
+        auto textDraw = [=] { ImGui::Text(winrt::to_string(text).c_str()); };
+        _beginCommands.emplace_back(textDraw);
+    }
+
+} // namespace winrt::WsiuRenderer::implementation
---
+++ WsiuRenderer/ImguiContext.h
@@ -0,0 +1,35 @@
+﻿#pragma once
+#include "ImguiContext.g.h"
+
+namespace winrt::WsiuRenderer::implementation
+{
+    struct ImguiContext : ImguiContextT<ImguiContext>
+    {
+        using EngineCore = winrt::WsiuRenderer::EngineCore;
+    public:
+        ImguiContext(EngineCore const& engineCore);
+        ~ImguiContext() override;
+
+        void InitializeWindow(hstring const& title);
+        uint64_t GetWindowID() const { return _windowID; }
+        void SetTitle(hstring const& title) const;
+        void Text(hstring const& text);
+
+    protected:
+        EngineCore _engineCore;
+
+    private:
+        void DrawCommands();
+
+        uint64_t _windowID = (std::numeric_limits<uint64_t>::max)();
+        std::vector<std::function<void()>> _beginCommands;
+        std::vector<std::function<void()>> _endCommands;
+    };
+}
+
+namespace winrt::WsiuRenderer::factory_implementation
+{
+    struct ImguiContext : ImguiContextT<ImguiContext, implementation::ImguiContext>
+    {
+    };
+}
