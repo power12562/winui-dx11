2dcc95ab7b11c8ed817cedd6b18b428f74f256d9
power12562 <kimsiwoo1005@gmail.com>
2026-03-15 12:20:30 +0900

    feature(editor): 에디터 fps 출력 테스트 추가

--- WsiuEditor/MainWindow.xaml.cs
+++ WsiuEditor/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using WsiuRenderer;
 using WsiuEngine.Core;
 
 // To learn more about WinUI, the WinUI project structure,
@@ -25,15 +26,45 @@ namespace WsiuEditor
     public sealed partial class MainWindow : Window
     {
         private Engine _engine;
+        private ImguiContext _inspector;
+        private UInt64 _frameCount;
+        private double _lastTime = 0;
+        private UInt64 _fps = 0;
+        private System.Diagnostics.Stopwatch _stopwatch = new();
 
         public MainWindow()
         {
             InitializeComponent();
             var hwnd = WinRT.Interop.WindowNative.GetWindowHandle(this);
             _engine = new Engine(hwnd, EnginePanel);
-            CompositionTarget.Rendering += (sender, args) => _engine.Update();
 
-            _engine.EditorWindowCreate("My First Editor");
+            _inspector = new(_engine.EngineCore);
+            _inspector.InitializeWindow("Inspector");
+
+            CompositionTarget.Rendering += (sender, args) => EditorLoop();
+
+            _stopwatch.Start();
+        }
+
+        private void EditorLoop()
+        {
+            InspectorDraw();
+            _engine.Update();
+        }
+
+        private void InspectorDraw()
+        {
+            ++_frameCount;
+            double currentTime = _stopwatch.Elapsed.TotalSeconds;
+            if (currentTime - _lastTime >= 1.0)
+            {
+                _fps = _frameCount;
+                _frameCount = 0;
+                _lastTime = currentTime;
+            }
+            _inspector.Text($"Current Frame: {_frameCount}");
+            _inspector.Text($"FPS: {_fps}");
+            _inspector.Text($"Engine Time: {DateTime.Now:HH:mm:ss}");
         }
     }
 }
