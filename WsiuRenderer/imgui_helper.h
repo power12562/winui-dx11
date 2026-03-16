b1dc078152fc09214a857413acb385f11f5fcd16
power12562 <kimsiwoo1005@gmail.com>
2026-03-16 12:11:37 +0900

    feature(commons): DragScalaNWithCallback 함수 추가

--- WsiuRenderer/imgui_helper.h
+++ WsiuRenderer/imgui_helper.h
@@ -1,2 +1,27 @@
 #pragma once
-ImGuiKey VirtualKeyToImGuiKey(WPARAM wParam);
+#include <imgui.h>
+namespace ImGuiHelper
+{
+    ImGuiKey VirtualKeyToImGuiKey(WPARAM wParam);
+
+    template <typename _scala_type> constexpr ImGuiDataType GetImGuiDataType()
+    {
+        if constexpr (std::is_same_v<float, _scala_type>)
+            return ImGuiDataType_Float;
+        else if constexpr (std::is_same_v<double, _scala_type>)
+            return ImGuiDataType_Double;
+        else
+            static_assert(false, "Unsupported type.");
+    }
+
+    template <typename _scala_type, typename _callback_type>
+    void DragScalaNWithCallback(const char* label, const _callback_type& callback, int n, _scala_type* val, float speed,
+                                _scala_type* min, _scala_type* max, const char* format, ImGuiSliderFlags flags)
+    {
+        constexpr ImGuiDataType type = GetImGuiDataType<_scala_type>();
+        if (ImGui::DragScalarN(label, type, val, n, speed, min, max, format, flags))
+        {
+            callback(*val);
+        }
+    }
+}
