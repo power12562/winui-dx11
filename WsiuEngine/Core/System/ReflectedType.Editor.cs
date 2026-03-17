79aad54b3d9e0b4c0b078dd0a206f4b5589c1a78
power12562 <kimsiwoo1005@gmail.com>
2026-03-17 10:31:33 +0900

    refactor(core): ReflectedType<T>가 class만 사용 가능하도록 개선

--- WsiuEngine/Core/System/ReflectedType.Editor.cs
+++ WsiuEngine/Core/System/ReflectedType.Editor.cs
@@ -1,42 +1,51 @@
-﻿using WsiuRenderer;
+﻿using System;
+using System.Numerics;
+using WsiuEngine.Extensions;
+using WsiuRenderer;
 namespace WsiuEngine.Core.System
 {
-    public static partial class ReflectedType<T>
+    public static partial class ReflectedType<T> where T : class
     {
         public static void DragFields(ImguiContext context, T target)
         {
             foreach (var field in Fields)
             {
+                object? value = field.Get(target);
+                if (value == null)
+                    continue;
+
+                string name = field.Name;
                 bool isReadOnly = field.Set == null;
                 if (isReadOnly)
                 {
-                    context.PushStyleVar(ImGuiStyleVar.Alpha, 0.5f);
+                    context.PushStyleVar(ImGuiStyleVar.Alpha, 0.70f);
                 }
 
-                if (field.Type == typeof(double))
+                Action drawAction = field.Type switch
                 {
-                    if (field.Get(target) is double f)
+                    var type when type == typeof(double) => () => context.DragDouble(name, (double)value, (v) =>
                     {
-                        context.DragDouble(field.Name, f, (val) =>
-                        {
-                            field.Set?.Invoke(target, val);
-                        });
-                    }
-                }
-                else if (field.Type == typeof(float))
-                {
-                    if (field.Get(target) is float f)
+                        field.Set?.Invoke(target, v);
+                    }),
+                    var type when type == typeof(float) => () => context.DragFloat(name, (float)value, (v) =>
                     {
-                        context.DragFloat(field.Name, f, (val) =>
-                        {
-                            field.Set?.Invoke(target, val);
-                        });
-                    }
-                }
-                else
-                {
-                    context.Text($"{field.Name}: {field.Get(target)}");
-                }
+                        field.Set?.Invoke(target, v);
+                    }),
+                    var type when type == typeof(Vector2) => () => context.DragVector2(name, (Vector2)value, (v) =>
+                    {
+                        field.Set?.Invoke(target, v);
+                    }),
+                    var type when type == typeof(Vector3) => () => context.DragVector3(name, (Vector3)value, (v) =>
+                    {
+                        field.Set?.Invoke(target, v);
+                    }),
+                    var type when type == typeof(Vector4) => () => context.DragVector4(name, (Vector4)value, (v) =>
+                    {
+                        field.Set?.Invoke(target, v);
+                    }),
+                    _ => () => context.Text($"{name}: {value}")
+                };
+                drawAction.Invoke();
 
                 if (isReadOnly)
                 {
