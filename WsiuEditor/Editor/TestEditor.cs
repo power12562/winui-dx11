9e5db0a10888bc1f0876dd4f0a53ce69b1350d5a
power12562 <kimsiwoo1005@gmail.com>
2026-03-19 17:52:01 +0900

    feature(core): EditorBase 상속받아서 구현

---
+++ WsiuEditor/Editor/TestEditor.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Numerics;
+using WsiuEngine.Core;
+using WsiuEngine.Core.System;
+using WsiuRenderer;
+
+namespace WsiuEditor.Editor
+{
+    internal class TestEditor : EditorBase
+    {
+        public TestEditor(Engine engine, UInt64 id) : base(engine, id)
+        {
+            _imguiContext.InitializeWindow("Test");
+            Name = "Test";
+        }
+
+        public override void Draw()
+        {
+            _imguiContext.TreeNodeEx("Test Methods", ImGuiTreeNodeFlags.None);
+            _imguiContext.PushStyleColor(ImGuiCol.Text, 0.4f, 0.7f, 1.0f, 1.0f);
+            ReflectedObject.DrawMethods(_imguiContext, _imguiContextClass.TestVector);
+            _imguiContext.PopStyleColor();
+            _imguiContext.TreePop();
+
+            ReflectedObject.DrawFields(_imguiContext, _imguiContextClass);
+        }
+
+        private readonly TestClassDraw _imguiContextClass = new();
+        class TestVectorDraw(TestClassDraw refer)
+        {
+            [SerializeField]
+            private readonly TestClassDraw testField = refer;
+
+            public Vector2 Vector2 = new();
+            public Vector3 Vector3 = new();
+            public Vector4 Vector4 = new();
+
+            [SerializeMethod]
+            public Vector2 AddVector2()
+            {
+                Vector2 += new Vector2(1, 1);
+                return Vector2;
+            }
+
+            [SerializeMethod]
+            public Vector2 AddVector2(Vector2 target)
+            {
+                Vector2 += target;
+                return Vector2;
+            }
+
+            [SerializeMethod]
+            private Vector2 SubVector2(Vector2 target)
+            {
+                Vector2 -= target;
+                return Vector2;
+            }
+        }
+
+        class TestClassDraw
+        {
+            public TestClassDraw()
+            {
+                TestVector = new(this);
+            }
+
+            public readonly TestVectorDraw TestVector;
+            public float TestFloat = 1.1f;
+            public Int16 TestInt16 = -16;
+            public Int32 TestInt32 = -32;
+            public Int64 TestInt64 = -64;
+            public UInt16 TestUInt16 = 16;
+            public UInt32 TestUInt32 = 32;
+            public UInt64 TestUInt64 = 64;
+        }
+    }
+}
---
+++ WsiuEditor/Editor/TimeEditor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using WsiuEngine.Core;
+using WsiuEngine.Core.System;
+using WsiuRenderer;
+
+namespace WsiuEditor.Editor
+{
+    internal class TimeEditor : EditorBase
+    {
+        public TimeEditor(Engine engine, UInt64 id) : base(engine, id) 
+        {       
+            _imguiContext.InitializeWindow("Time");
+            Name = "Time";
+        }
+
+        public override void Draw()
+        {
+            ImguiContext.SettingFloat(0.01f, 0, 0, "%.3f", ImGuiSliderFlags.None);
+            ImguiContext.SettingDouble(0.01f, 0, 0, "%.3f", ImGuiSliderFlags.None);
+            ReflectedObject.DrawFields(_imguiContext, Engine.Time);
+        }
+
+    }
+}
