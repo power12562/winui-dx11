8001b94ce39ca16b8e4fe241134ce940ad6d2990
power12562 <kimsiwoo1005@gmail.com>
2026-03-19 17:51:46 +0900

    feature(editor):ImguiContext 사용하는 EditorBase 추가

---
+++ WsiuEditor/Editor/EditorBase.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using WsiuEngine.Core;
+using WsiuRenderer;
+
+namespace WsiuEditor.Editor
+{
+    internal abstract class EditorBase : IEditor
+    {
+        public UInt64 ID 
+        {
+            get => _id;
+        }
+        private readonly UInt64 _id;
+
+        public string Name 
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+                _imguiContext.SetTitle(GetImguiTitle());
+            }
+        }
+        private string _name = string.Empty;
+
+        public bool Active 
+        {
+            get => _imguiContext.GetActive();
+            set => _imguiContext.SetActive(value);
+        }
+        protected string GetImguiTitle() { return $"{_name}##{ID}"; }
+
+        public abstract void Draw();
+
+        protected readonly ImguiContext _imguiContext;
+        protected EditorBase(Engine engine, UInt64 id)
+        {
+            _imguiContext = new(engine.EngineCore);
+            _id = id;
+        }
+    }
+}
