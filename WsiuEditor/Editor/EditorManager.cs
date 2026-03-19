30d40d527a5398abbafa8bef24b4fa36c5bcf86a
power12562 <kimsiwoo1005@gmail.com>
2026-03-19 15:59:11 +0900

    feature(core): EditorManager 추가

---
+++ WsiuEditor/Editor/EditorManager.cs
@@ -0,0 +1,20 @@
+﻿using WsiuEngine.Core;
+using WsiuEngine.Extensions;
+
+namespace WsiuEditor.Editor
+{
+    internal partial class EditorManager
+    {
+        public EditorManager(Engine engine)
+        {
+            _timeDebugUI = ImguiContextExtensions.CreateImguiContext(engine.EngineCore, "Time", ContextType.Default);
+            _test = ImguiContextExtensions.CreateImguiContext(engine.EngineCore, "Debug", ContextType.Default);
+        }
+
+        internal void DrawEditors()
+        {
+            DrawTimeDebug();
+            DrawTestDebug();
+        }
+    }
+}
