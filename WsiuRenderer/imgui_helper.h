20998f0917aa7420ef8d416eef5ef622ff123faf
power12562 <kimsiwoo1005@gmail.com>
2026-03-19 14:28:06 +0900

    feature(core):GetImguiOutputFormat 함수 추가

--- WsiuRenderer/imgui_helper.h
+++ WsiuRenderer/imgui_helper.h
@@ -1,15 +1,70 @@
 #pragma once
+#include <stdint.h>
 #include <imgui.h>
 namespace ImGuiHelper
 {
     ImGuiKey VirtualKeyToImGuiKey(WPARAM wParam);
 
-    template <typename _scala_type> constexpr ImGuiDataType GetImGuiDataType()
+    template <typename T>
+    constexpr const char* GetImguiOutputFormat()
+    {
+        // 부동 소수점
+        if constexpr (std::is_same_v<T, float>)
+            return "%.3f";
+        else if constexpr (std::is_same_v<T, double>)
+            return "%.6lf";
+
+        // 8비트 정수 (char / byte)
+        else if constexpr (std::is_same_v<T, char>)
+            return "%d";
+        else if constexpr (std::is_same_v<T, unsigned char>)
+            return "%u";
+
+        // 16비트 정수 (short)
+        else if constexpr (std::is_same_v<T, short>)
+            return "%hd";
+        else if constexpr (std::is_same_v<T, unsigned short>)
+            return "%hu";
+
+        // 32비트 정수 (int)
+        else if constexpr (std::is_same_v<T, int>)
+            return "%d";
+        else if constexpr (std::is_same_v<T, unsigned int>)
+            return "%u";
+
+        // 64비트 정수 (long long)
+        else if constexpr (std::is_same_v<T, long long>)
+            return "%lld";
+        else if constexpr (std::is_same_v<T, unsigned long long>)
+            return "%llu";
+
+        else
+            static_assert(false, "Unsupported type.");
+    }
+
+    template <typename _scala_type> 
+    constexpr ImGuiDataType GetImGuiDataType()
     {
         if constexpr (std::is_same_v<float, _scala_type>)
             return ImGuiDataType_Float;
         else if constexpr (std::is_same_v<double, _scala_type>)
             return ImGuiDataType_Double;
+        else if constexpr (std::is_same_v<int16_t, _scala_type>)
+
+            return ImGuiDataType_S16;
+        else if constexpr (std::is_same_v<uint16_t, _scala_type>)
+            return ImGuiDataType_U16;
+        else if constexpr (std::is_same_v<int32_t, _scala_type>)
+
+            return ImGuiDataType_S32;
+        else if constexpr (std::is_same_v<uint32_t, _scala_type>)
+            return ImGuiDataType_U32;
+
+        else if constexpr (std::is_same_v<int64_t, _scala_type>)
+            return ImGuiDataType_S64;
+        else if constexpr (std::is_same_v<uint64_t, _scala_type>)
+            return ImGuiDataType_U64;
+
         else
             static_assert(false, "Unsupported type.");
     }
