cad477d4112e844596a7f92af273430a620f2f14
power12562 <kimsiwoo1005@gmail.com>
2026-03-19 17:50:51 +0900

    refactor(editor): EditorManager가 IEditor 타입 생성 및 List 관리 체계로 변경

---
+++ WsiuEditor/System/EditorManager.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using WsiuEditor.Editor;
+using WsiuEngine.Core;
+
+namespace WsiuEditor.System
+{
+    internal partial class EditorManager(Engine engine)
+    {
+        private readonly Engine _engine = engine;
+        private readonly List<IEditor> _editors = [];
+        private readonly Dictionary<Type, UInt64> _idCounter = [];
+
+        public void CreateEditor<T>(Engine engine) where T : IEditor
+        {
+            if (editorProvider.TryGetValue(typeof(T), out var func))
+            {
+                IEditor iEditor = func(engine, GetEditorId(typeof(T)));
+                if (iEditor is T editor)
+                {
+                    _editors.Add(editor);
+                }
+            }
+        }
+
+        public void CreateEditor(Type type, Engine engine)
+        {
+            if (editorProvider.TryGetValue(type, out var func))
+            {
+                IEditor iEditor = func(engine, GetEditorId(type));
+                _editors.Add(iEditor);
+            }
+        }
+
+        internal void DrawEditors()
+        {
+            foreach (IEditor editor in _editors)
+            {
+                editor.Draw();
+            }
+        }
+
+        private UInt64 GetEditorId(Type type)
+        {
+            _idCounter.TryGetValue(type, out UInt64 id);
+            _idCounter[type] = id + 1;
+            return id;
+        }
+    }
+}
---
+++ WsiuEditor/System/EditorManager.static.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WsiuEditor.Editor;
+using WsiuEngine.Core;
+
+namespace WsiuEditor.System
+{
+    internal partial class EditorManager
+    {
+        public delegate IEditor ActionCreateEditor(Engine engine, UInt64 id);
+
+        private static readonly Dictionary<Type, ActionCreateEditor> editorProvider = [];
+        public static void RegisterEditors()
+        {
+            var types = AppDomain.CurrentDomain.GetAssemblies()
+                .SelectMany(s => s.GetTypes())
+                .Where(p => typeof(IEditor).IsAssignableFrom(p)
+                         && !p.IsInterface
+                         && !p.IsAbstract);
+
+            foreach (var type in types)
+            {
+                editorProvider[type] = (engine, id) =>
+                {
+                    object? instance = Activator.CreateInstance(type, engine, id);
+                    if (instance is IEditor editor)
+                    {
+                        return editor;
+                    }
+                    throw new InvalidOperationException($"{type.Name}은 IEditor를 구현하지 않았습니다.");
+                };
+            }
+        }
+    }
+}
