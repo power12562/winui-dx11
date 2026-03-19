b5cc6f76e9e2b7ea5750efa0ea0dc5836ea1d5b9
power12562 <kimsiwoo1005@gmail.com>
2026-03-19 21:34:55 +0900

    feature(editor): MainMenuItem에 Windos 탭 추가

--- WsiuEditor/System/EditorManager.MainMenuBar.cs
+++ WsiuEditor/System/EditorManager.MainMenuBar.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using ABI.WsiuRenderer;
+using System;
 using System.Collections.Generic;
+using WsiuEditor.Editor;
 using WsiuEngine.Core;
 using WsiuRenderer;
 
@@ -7,9 +9,33 @@ namespace WsiuEditor.System
 {
     internal partial class EditorManager
     {
-        void DrawMainMenuBar()
+        private void DrawMainMenuBar()
         {
+            _imguiContext.BeginMainMenuBar();
+            DrawWindowMenu();
+            _imguiContext.EndMainMenuBar();
+        }
 
+        private void DrawWindowMenu()
+        {
+            _imguiContext.BeginMenu("Window");
+            DrawCreateEditorMenuItems();
+            _imguiContext.EndMenu();
+        }
+
+        private void DrawCreateEditorMenuItems()
+        {
+            foreach (var editorType in editorProvider)
+            {
+                Type type = editorType.Key;
+                ActionCreateEditor createEditor = editorType.Value;
+                _imguiContext.MenuItem(type.Name, () =>
+                {
+                    IEditor editor = createEditor(_engine, AddEditorId(type));
+                    editor.SetDisableCallback(CleanUpEditors);
+                    _editors.Add(editor);                
+                });
+            }
         }
     }
 }
