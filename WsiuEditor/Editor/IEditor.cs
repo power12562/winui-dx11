2d9afb4ded52eda333bbff728a87bf68ac6e4574
power12562 <kimsiwoo1005@gmail.com>
2026-03-19 17:51:20 +0900

    feature(core): IEditor 인터페이스 추가

---
+++ WsiuEditor/Editor/IEditor.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace WsiuEditor.Editor
+{
+    internal interface IEditor
+    {
+        UInt64 ID { get; }
+        string Name { get; set; }
+        bool Active { get; set; }
+        public void Draw();
+    }
+}
