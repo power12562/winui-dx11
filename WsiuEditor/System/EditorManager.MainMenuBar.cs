4e771eb593a4c4e5e3d671833624f50768dd6feb
power12562 <kimsiwoo1005@gmail.com>
2026-03-19 19:15:04 +0900

    feature(core): EditorManager에 MainMenuBar 기능 추가

---
+++ WsiuEditor/System/EditorManager.MainMenuBar.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using WsiuEngine.Core;
+using WsiuRenderer;
+
+namespace WsiuEditor.System
+{
+    internal partial class EditorManager
+    {
+        void DrawMainMenuBar()
+        {
+
+        }
+    }
+}
--- WsiuEditor/System/EditorManager.cs
+++ WsiuEditor/System/EditorManager.cs
@@ -2,20 +2,22 @@
 using System.Collections.Generic;
 using WsiuEditor.Editor;
 using WsiuEngine.Core;
+using WsiuRenderer;
 
 namespace WsiuEditor.System
 {
     internal partial class EditorManager(Engine engine)
     {
         private readonly Engine _engine = engine;
+        private readonly ImguiContext _imguiContext = new(engine.EngineCore);
         private readonly List<IEditor> _editors = [];
-        private readonly Dictionary<Type, UInt64> _idCounter = [];
+        private readonly Dictionary<Type, UInt64> _editorIdCounter = [];
 
-        public void CreateEditor<T>(Engine engine) where T : IEditor
+        public void CreateEditor<T>() where T : IEditor
         {
             if (editorProvider.TryGetValue(typeof(T), out var func))
             {
-                IEditor iEditor = func(engine, GetEditorId(typeof(T)));
+                IEditor iEditor = func(_engine, AddEditorId(typeof(T)));
                 if (iEditor is T editor)
                 {
                     _editors.Add(editor);
@@ -23,11 +25,11 @@ namespace WsiuEditor.System
             }
         }
 
-        public void CreateEditor(Type type, Engine engine)
+        public void CreateEditor(Type type)
         {
             if (editorProvider.TryGetValue(type, out var func))
             {
-                IEditor iEditor = func(engine, GetEditorId(type));
+                IEditor iEditor = func(_engine, AddEditorId(type));
                 _editors.Add(iEditor);
             }
         }
@@ -36,14 +38,15 @@ namespace WsiuEditor.System
         {
             foreach (IEditor editor in _editors)
             {
+                DrawMainMenuBar();
                 editor.Draw();
             }
         }
 
-        private UInt64 GetEditorId(Type type)
+        private UInt64 AddEditorId(Type type)
         {
-            _idCounter.TryGetValue(type, out UInt64 id);
-            _idCounter[type] = id + 1;
+            _editorIdCounter.TryGetValue(type, out UInt64 id);
+            _editorIdCounter[type] = id + 1;
             return id;
         }
     }
