0d053716f34e4229667be1aee53e09c0e203797c
power12562 <kimsiwoo1005@gmail.com>
2026-03-20 13:08:27 +0900

    feature(core): 화면 생성 후 최대화 하도록 기능 추가

--- WsiuEditor/MainWindow.xaml.cs
+++ WsiuEditor/MainWindow.xaml.cs
@@ -1,9 +1,12 @@
-using System;
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Media;
-using WsiuEngine.Core;
-using WsiuEditor.System;
+using System;
+using Windows.Graphics;
 using WsiuEditor.Editor;
+using WsiuEditor.System;
+using WsiuEngine.Core;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -20,7 +23,7 @@ namespace WsiuEditor
 
         public MainWindow()
         {
-            InitializeComponent();
+            InitializeComponent(); 
             var hwnd = WinRT.Interop.WindowNative.GetWindowHandle(this);
             _engine = new Engine(hwnd, EnginePanel);
 
@@ -28,12 +31,34 @@ namespace WsiuEditor
             _editorManager = new(_engine);
 
             CompositionTarget.Rendering += (sender, args) => EditorLoop();
+            if (Content is FrameworkElement frameworkElement)
+            {
+                frameworkElement.Loaded += (obj, eventArgs) => SetWindowToFullWorkArea();
+            }    
         }
 
         private void EditorLoop()
         {
             _editorManager.Draw();
             _engine.Update();
+        }
+
+        private void SetWindowToFullWorkArea()
+        {
+            IntPtr hWnd = WinRT.Interop.WindowNative.GetWindowHandle(this);
+            WindowId windowId = Win32Interop.GetWindowIdFromWindow(hWnd);
+            AppWindow appWindow = AppWindow.GetFromWindowId(windowId);
+            if (appWindow != null)
+            {
+                DisplayArea displayArea = DisplayArea.GetFromWindowId(windowId, DisplayAreaFallback.Primary);
+                RectInt32 workArea = displayArea.WorkArea;
+                appWindow.MoveAndResize(new RectInt32(
+                    workArea.X,
+                    workArea.Y,
+                    workArea.Width,
+                    workArea.Height
+                ));
+            }
         }
     }
 }
