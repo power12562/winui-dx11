3f7c8bcf5ec96586dbdfe6e317cb4fcf767facdd
power12562 <kimsiwoo1005@gmail.com>
2026-03-20 11:25:28 +0900

    refactor(code): 컨밴션에 맞게 정렬

--- WsiuRenderer/EditorCommands.cpp
+++ WsiuRenderer/EditorCommands.cpp
@@ -1,6 +1,9 @@
 #include "pch.h"
 #include "EditorCommands.h"
-EditorCommands::EditorCommands(const winrt::hstring& title) { _title = winrt::to_string(title); }
+EditorCommands::EditorCommands(const winrt::hstring& title)
+{
+    _title = winrt::to_string(title);
+}
 
 EditorCommands::~EditorCommands() = default;
 
@@ -19,23 +22,41 @@ void EditorCommands::OnDraw()
         _endFunction();
 }
 
-void EditorCommands::SetTitle(const winrt::hstring& title) { _title = winrt::to_string(title); }
+void EditorCommands::SetTitle(const winrt::hstring& title)
+{
+    _title = winrt::to_string(title);
+}
 
-void EditorCommands::SetActive(bool active) 
-{ 
-    _active = active; 
+void EditorCommands::SetActive(bool active)
+{
+    _active = active;
     if (_active == false && _disableFunction)
     {
-        _disableFunction();    
+        _disableFunction();
     }
 }
 
-bool EditorCommands::GetActive() const { return _active; }
+bool EditorCommands::GetActive() const
+{
+    return _active;
+}
 
-void EditorCommands::BeginCallback(const std::function<void()>& begin) { _beginFunction = begin; }
+void EditorCommands::BeginCallback(const std::function<void()>& begin)
+{
+    _beginFunction = begin;
+}
 
-void EditorCommands::DrawCallback(const std::function<void()>& draw) { _drawFunction = draw; }
+void EditorCommands::DrawCallback(const std::function<void()>& draw)
+{
+    _drawFunction = draw;
+}
 
-void EditorCommands::EndCallback(const std::function<void()>& end) { _endFunction = end; }
+void EditorCommands::EndCallback(const std::function<void()>& end)
+{
+    _endFunction = end;
+}
 
-void EditorCommands::DisableCallback(const std::function<void()>& disable) { _disableFunction = disable; }
+void EditorCommands::DisableCallback(const std::function<void()>& disable)
+{
+    _disableFunction = disable;
+}
