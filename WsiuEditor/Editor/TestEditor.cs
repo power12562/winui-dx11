10aa26d4df76ffa9f8c1efb09dcbcb170b3a5e44
power12562 <kimsiwoo1005@gmail.com>
2026-03-23 17:27:12 +0900

    feature(test): 신규 어트리뷰트 테스트 코드 추가

--- WsiuEditor/Editor/TestEditor.cs
+++ WsiuEditor/Editor/TestEditor.cs
@@ -18,29 +18,18 @@ namespace WsiuEditor.Editor
 
         public override void Draw()
         {
-            _imguiContext.TreeNodeEx("TestVectorDraw Methods", ImGuiTreeNodeFlags.None);
+            _imguiContext.Text("Fields");
+            ReflectionObject.DrawFields(_imguiContext, _testClass);
+            _imguiContext.Separator();
+            _imguiContext.Text("Methods");
             _imguiContext.PushStyleColor(ImGuiCol.Text, 0.4f, 0.7f, 1.0f, 1.0f);
-            ReflectionObject.DrawMethods(_imguiContext, _imguiContextClass.TestVector);
+            ReflectionObject.DrawMethods(_imguiContext, _testClass);
             _imguiContext.PopStyleColor();
-            _imguiContext.TreePop();
-
-            _imguiContext.TreeNodeEx("TestClassDraw Fields##1", ImGuiTreeNodeFlags.None);
-            ReflectionObject.DrawFields(_imguiContext, _imguiContextClass);
-            _imguiContext.TreePop();
-            _imguiContext.TreeNodeEx("TestClassDraw Fields##2", ImGuiTreeNodeFlags.None);
-            ReflectionObject.DrawFields(_imguiContext, _imguiContextClass);
-            _imguiContext.TreePop();
         }
 
-        private readonly TestClassDraw _imguiContextClass = new();
+        private readonly TestClassDraw _testClass = new();
         class TestClassDraw
         {
-            public TestClassDraw()
-            {
-                TestVector = new(this);
-            }
-
-            public readonly TestVectorDraw TestVector;
             public float TestFloat = 1.1f;
             public Int16 TestInt16 = -16;
             public Int32 TestInt32 = -32;
@@ -48,37 +37,27 @@ namespace WsiuEditor.Editor
             public UInt16 TestUInt16 = 16;
             public UInt32 TestUInt32 = 32;
             public UInt64 TestUInt64 = 64;
-            public string TestSerialize = "";
-        }
+            public Vector2 TestVector2 = new();
+            public Vector3 TestVector3 = new();
+            public Vector4 TestVector4 = new();
 
-        class TestVectorDraw(TestClassDraw refer)
-        {
-            [SerializeField]
-            private readonly TestClassDraw testField = refer;
-           
-            public Vector2 Vector2 = new();
-            public Vector3 Vector3 = new();
-            public Vector4 Vector4 = new();
+            [HideInInspector]
+            public string TestString = "";
+
+            [MultilineStringField(Height = 100f)]
+            public string TestSerialize => _testSerialize;
+            private string _testSerialize = "";
+
 
             [SerializeMethod]
-            public Vector2 AddVector2()
+            public void Serialize()
             {
-                Vector2 += new Vector2(1, 1);
-                return Vector2;
+                _testSerialize = ReflectionObject.SerializeToJson(this);
             }
-
             [SerializeMethod]
-            public Vector2 AddVector2(Vector2 target)
+            public void Deserialize()
             {
-                Vector2 += target;
-                return Vector2;
-            }
-
-            [SerializeMethod]
-            private Vector2 SubVector2(Vector2 target)
-            {
-                Vector2 -= target;
-                return Vector2;
+                ReflectionObject.DeserializeFromJson(this, _testSerialize);
             }
         }
     }
