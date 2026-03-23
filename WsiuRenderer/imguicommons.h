8cfff9f1c4dc52febe8f842e8aa547b080672b37
power12562 <kimsiwoo1005@gmail.com>
2026-03-23 13:42:16 +0900

    chore(include):imgui_stdlib.h 추가

--- WsiuRenderer/imguicommons.h
+++ WsiuRenderer/imguicommons.h
@@ -1,5 +1,6 @@
 #pragma once
-#include <imgui.h>
-#include <imgui_impl_win32.h>
-#include <imgui_impl_dx11.h>
+#include "imgui.h"
+#include "imgui_impl_win32.h"
+#include "imgui_impl_dx11.h"
+#include "imgui_stdlib.h"
 #include "imgui_helper.h"
