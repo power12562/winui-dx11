132a953110071c82e415304e69041fc345471847
power12562 <kimsiwoo1005@gmail.com>
2026-03-24 12:57:55 +0900

    feature(core): 이전 화면 사이즈인 RestoreBounds 기능 추가

--- WsiuEngine/Core/System/Screen.cs
+++ WsiuEngine/Core/System/Screen.cs
@@ -1,17 +1,34 @@
-﻿using Microsoft.UI.Windowing;
+﻿using Microsoft.UI;
+using Microsoft.UI.Windowing;
+using System.Runtime.InteropServices;
 
 namespace WsiuEngine.Core.System
 {
-    public class Screen
+    public partial class Screen
     {
+        private readonly nint _hwnd;
+        private readonly WindowId _windowID;
         private readonly AppWindow _appWindow;
-        public Screen(AppWindow appWindow)
+        public Screen(nint hwnd)
         {
-            _appWindow = appWindow;
+            _hwnd = hwnd;
+            _windowID = Win32Interop.GetWindowIdFromWindow(hwnd);
+            _appWindow = AppWindow.GetFromWindowId(_windowID);
         }
 
-        public int Width { get => _appWindow.Size.Width; }
-        public int Height { get => _appWindow.Size.Height; }
+        public struct Bounds(int x, int y, int width, int height)
+        {
+            public int X = x;
+            public int Y = y;
+            public int Width = width;
+            public int Height = height;
+        }
+
+        public int Width => _appWindow.Size.Width;
+        public int Height => _appWindow.Size.Height;
+
+        public int PosX => _appWindow.Position.X;
+        public int PosY => _appWindow.Position.Y;
 
         public bool IsMaximized
         {
@@ -21,6 +38,42 @@ namespace WsiuEngine.Core.System
         public bool IsFullScreen
         {
             get => _appWindow.Presenter.Kind == AppWindowPresenterKind.FullScreen;
+        }
+
+        public void Move(int x, int y)
+        {
+            _appWindow.Move(new(x, y));
+        }
+
+        public void Resize(int width, int height)
+        {
+            _appWindow.ResizeClient(new(width, height));
+        }
+
+        public void Maximize()
+        {
+            if (_appWindow.Presenter is OverlappedPresenter overlapped)
+                overlapped.Maximize();
+        }
+
+        public Bounds RestoreBounds
+        { 
+            get
+            {
+                WINDOWPLACEMENT wp = new()
+                {
+                    length = Marshal.SizeOf<WINDOWPLACEMENT>()
+                };
+                if (GetWindowPlacement(_hwnd, ref wp))
+                {
+                    int x = wp.rcNormalPosition.left;
+                    int y = wp.rcNormalPosition.top;
+                    int width = wp.rcNormalPosition.right - wp.rcNormalPosition.left;
+                    int height = wp.rcNormalPosition.bottom - wp.rcNormalPosition.top;
+                    return new(x, y, width, height);
+                }
+                return new(PosX, PosY, Width, Height);
+            }
         }
     }
 }
