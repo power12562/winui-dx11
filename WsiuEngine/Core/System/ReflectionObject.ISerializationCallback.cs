49f2c4e8886b9dc7624f0465dde2ae5d410c7926
power12562 <kimsiwoo1005@gmail.com>
2026-03-24 11:02:58 +0900

    feature(core):ISerializationCallback 추가

---
+++ WsiuEngine/Core/System/ReflectionObject.ISerializationCallback.cs
@@ -0,0 +1,11 @@
+﻿namespace WsiuEngine.Core.System
+{
+    public static partial class ReflectionObject
+    {
+        public interface ISerializationCallback
+        {
+            void OnBeforeSerialize();
+            void OnAfterDeserialize();
+        }
+    }
+}
