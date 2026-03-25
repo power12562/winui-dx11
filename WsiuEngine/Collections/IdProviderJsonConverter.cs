84549becf08ef73d13c856a55c8264ebf37f430f
power12562 <kimsiwoo1005@gmail.com>
2026-03-25 18:27:08 +0900

    feature(core): IdProvider의 JsonConverter 추가

---
+++ WsiuEngine/Collections/IdProviderJsonConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using WsiuEngine.Core.System;
+
+namespace WsiuEngine.Collections
+{
+    public class IdProviderJsonConverter : JsonConverter<IdProvider>
+    {
+        public override IdProvider? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {        
+            string? json = reader.GetString();
+            if (string.IsNullOrEmpty(json))
+                return null;
+
+            IdProvider idProvider = new();
+            ReflectionObject.DeserializeFromJson(idProvider, json, options);
+            return idProvider;
+        }
+
+        public override void Write(Utf8JsonWriter writer, IdProvider value, JsonSerializerOptions options)
+        {
+            string jsonString = ReflectionObject.SerializeToJson(value, options);
+            writer.WriteStringValue(jsonString);
+        }
+    }
+}
