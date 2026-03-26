f73f40d17362dc96878f8f11c45f9288b70ba8ea
power12562 <kimsiwoo1005@gmail.com>
2026-03-26 14:25:01 +0900

    feature(core): 싱글톤용 인터페이스 추가

---
+++ WsiuEngine/Core/Interfaces/ISingleton.cs
@@ -0,0 +1,10 @@
+﻿namespace WsiuEngine.Core.Interfaces
+{
+    /// <summary>
+    /// Engine 클래스의 싱글톤 인스턴스 추적용 인터페이스입니다. <br/>
+    /// 클래스 외부에서는 [ClassName.Funtion()] 형태의 정적 호출을 원칙으로 하며, <br/>
+    /// 본 인터페이스는 Engine에 인스턴스 전달이 필요한 특수 상황에서 제한적으로 사용합니다. <br/>
+    /// </summary>
+    public interface ISingleton
+    {}
+}
