b8b5a89f01d78e68ca69f981d8821bc3235f3d1d
power12562 <kimsiwoo1005@gmail.com>
2026-03-26 17:21:18 +0900

    feature(editor) Screen 클래스 디버깅용 Editor 추가

---
+++ WsiuEditor/Editor/ScreenEditor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WsiuEditor.Editor.Base;
+using WsiuEditor.Interfaces;
+using WsiuEngine.Core;
+using WsiuEngine.Core.System;
+using WsiuRenderer;
+
+namespace WsiuEditor.Editor
+{
+    [SingletonEditor]
+    public class ScreenEditor : ImguiEditorBase
+    {
+        private const string editorName = "Screen";
+
+        public ScreenEditor(Engine engine, ulong id) : base(engine, id)
+        {
+            _imguiContext.InitializeWindowClosable(editorName);
+            Name = editorName;
+        }
+
+        public override void Draw()
+        {
+            ImguiContext.SettingInt32(1.0f, 0, 0, "%d", ImGuiSliderFlags.None);
+            ReflectionObject.DrawStaticFields<Screen>(_imguiContext);
+        }
+    }
+}
