beb9f9c3878941d51865439df13e966bce2c5bb4
power12562 <kimsiwoo1005@gmail.com>
2026-03-26 14:42:05 +0900

    refactor(core):싱글톤 컨벤션에 맞게 리팩토링

--- WsiuEngine/Core/System/Screen.cs
+++ WsiuEngine/Core/System/Screen.cs
@@ -1,20 +1,28 @@
 ﻿using Microsoft.UI;
 using Microsoft.UI.Windowing;
 using System.Runtime.InteropServices;
+using Windows.Graphics;
 
 namespace WsiuEngine.Core.System
 {
     public partial class Screen
     {
-        private readonly nint _hwnd;
-        private readonly WindowId _windowID;
-        private readonly AppWindow _appWindow;
-        public Screen(nint hwnd)
+        private static Screen instance = null!;
+        internal static void Initialize(nint hwnd)
+        {
+            instance = new Screen(hwnd);
+        }
+
+        private Screen(nint hwnd)
         {
             _hwnd = hwnd;
             _windowID = Win32Interop.GetWindowIdFromWindow(hwnd);
             _appWindow = AppWindow.GetFromWindowId(_windowID);
         }
+
+        private readonly nint _hwnd;
+        private readonly WindowId _windowID;
+        private readonly AppWindow _appWindow;
 
         public struct Bounds(int x, int y, int width, int height)
         {
@@ -24,56 +32,58 @@ namespace WsiuEngine.Core.System
             public int Height = height;
         }
 
-        public int Width => _appWindow.Size.Width;
-        public int Height => _appWindow.Size.Height;
+        public static int Width => instance.InternalGetAppWindowSize().Width;
+        public static int Height => instance.InternalGetAppWindowSize().Height;
+        internal SizeInt32 InternalGetAppWindowSize() => _appWindow.Size;   
 
-        public int PosX => _appWindow.Position.X;
-        public int PosY => _appWindow.Position.Y;
+        public static int PosX => instance.InternalGetAppWindowPosition().X;
+        public static int PosY => instance.InternalGetAppWindowPosition().Y;
 
-        public bool IsMaximized
-        {
-            get => _appWindow.Presenter is OverlappedPresenter { State: OverlappedPresenterState.Maximized };
-        }
+        internal PointInt32 InternalGetAppWindowPosition() => _appWindow.Position;
 
-        public bool IsFullScreen
-        {
-            get => _appWindow.Presenter.Kind == AppWindowPresenterKind.FullScreen;
-        }
+        public static bool IsMaximized => instance.IsMaximizedWindow();
+        public bool IsMaximizedWindow() => _appWindow.Presenter is OverlappedPresenter { State: OverlappedPresenterState.Maximized };
 
-        public void Move(int x, int y)
+        public static bool IsFullScreen => instance.IsFullScreenWindow();
+        public bool IsFullScreenWindow() => _appWindow.Presenter.Kind == AppWindowPresenterKind.FullScreen;
+
+
+        public static void Move(int x, int y) => instance.InternalMove(x, y);
+        internal void InternalMove(int x, int y)
         {
             _appWindow.Move(new(x, y));
         }
 
-        public void Resize(int width, int height)
+        public static void Resize(int width, int height) => instance.InternalResize(width, height);
+        internal void InternalResize(int width, int height)
         {
             _appWindow.ResizeClient(new(width, height));
         }
 
-        public void Maximize()
+   
+        public static void Maximize() => instance.InternalMaximize();
+        internal void InternalMaximize()
         {
             if (_appWindow.Presenter is OverlappedPresenter overlapped)
                 overlapped.Maximize();
         }
 
-        public Bounds RestoreBounds
-        { 
-            get
+        public static Bounds RestoreBounds => instance.InternalGetRestoreBounds();
+        internal Bounds InternalGetRestoreBounds()
+        {
+            WINDOWPLACEMENT wp = new()
             {
-                WINDOWPLACEMENT wp = new()
-                {
-                    length = Marshal.SizeOf<WINDOWPLACEMENT>()
-                };
-                if (GetWindowPlacement(_hwnd, ref wp))
-                {
-                    int x = wp.rcNormalPosition.left;
-                    int y = wp.rcNormalPosition.top;
-                    int width = wp.rcNormalPosition.right - wp.rcNormalPosition.left;
-                    int height = wp.rcNormalPosition.bottom - wp.rcNormalPosition.top;
-                    return new(x, y, width, height);
-                }
-                return new(PosX, PosY, Width, Height);
+                length = Marshal.SizeOf<WINDOWPLACEMENT>()
+            };
+            if (GetWindowPlacement(_hwnd, ref wp))
+            {
+                int x = wp.rcNormalPosition.left;
+                int y = wp.rcNormalPosition.top;
+                int width = wp.rcNormalPosition.right - wp.rcNormalPosition.left;
+                int height = wp.rcNormalPosition.bottom - wp.rcNormalPosition.top;
+                return new(x, y, width, height);
             }
+            return new(PosX, PosY, Width, Height);
         }
     }
 }
