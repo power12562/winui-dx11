504da512ca33dc017573b053e1d16a4f22209c1d
power12562 <kimsiwoo1005@gmail.com>
2026-03-26 11:55:09 +0900

    feature(core): Task용 Extensions Forget 기능 추가

---
+++ WsiuEngine/Extensions/TaskExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace WsiuEngine.Extensions
+{
+    public static class TaskExtensions
+    {
+        public static void Forget(this Task task)
+        {
+            if (task == null)
+                return;
+
+            Task.Run(async () =>
+            {
+                try
+                {
+                    await task;
+                }
+                catch (Exception ex)
+                {
+                    //TODO: 이후 엔진 전용 로그로 교체해야함.
+                    Debug.WriteLine($"[Task Error] {ex.Message}");
+                    Debug.WriteLine(ex.StackTrace);
+                    if(Debugger.IsAttached) 
+                        Debugger.Break();
+                }
+            });
+        }
+
+    }
+}
