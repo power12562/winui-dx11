794cdf332892aa331b0fa0d0c1c70ba91108eea7
power12562 <kimsiwoo1005@gmail.com>
2026-03-27 10:38:33 +0900

    feature(core): WindowService 추가
    CreateFileOpenPicker - FileOpenPicker 객체 초기화 후 생성 반환
    
    CreateFileSavePicker - FileSavePicker  객체 초기화 후 생성 반환

---
+++ WsiuEngine/Core/System/WindowService.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.UI;
+using Microsoft.Windows.Storage.Pickers;
+using WinRT.Interop;
+
+namespace WsiuEngine.Core.System
+{
+    public class WindowService
+    {
+        private static WindowService instance = null!;
+
+        internal static void Initialize(nint hwnd)
+        {
+            instance = new WindowService(hwnd);
+        }
+
+        private nint _hwnd;
+        private WindowId _windowId;
+        private WindowService(nint hwnd)
+        {
+            _hwnd = hwnd;
+            _windowId = Win32Interop.GetWindowIdFromWindow(hwnd);
+        }
+
+        public static FileOpenPicker CreateFileOpenPicker()
+        {
+            return instance.InternalCreateFileOpenPicker();
+        }
+
+        internal FileOpenPicker InternalCreateFileOpenPicker()
+        {
+            FileOpenPicker picker = new(_windowId);
+            InitializeWithWindow.Initialize(picker, _hwnd);
+            return picker;
+        }
+
+        public static FileSavePicker CreateFileSavePicker()
+        {
+            return instance.InternalCreateFileSavePicker();
+        }
+
+        internal FileSavePicker InternalCreateFileSavePicker()
+        {
+            FileSavePicker picker = new(_windowId);
+            InitializeWithWindow.Initialize(picker, _hwnd);
+            return picker;
+        }   
+    }
+}
