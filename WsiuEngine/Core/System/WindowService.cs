32b95ac090a45691243cf0d340176d3c71e57b98
power12562 <kimsiwoo1005@gmail.com>
2026-03-27 12:27:56 +0900

    feature(core): GetOpenFilePathAsync, GetSaveFilePathAsync 추가

--- WsiuEngine/Core/System/WindowService.cs
+++ WsiuEngine/Core/System/WindowService.cs
@@ -1,5 +1,10 @@
 ﻿using Microsoft.UI;
 using Microsoft.Windows.Storage.Pickers;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Storage;
 using WinRT.Interop;
 
 namespace WsiuEngine.Core.System
@@ -25,7 +30,6 @@ namespace WsiuEngine.Core.System
         {
             return instance.InternalCreateFileOpenPicker();
         }
-
         internal FileOpenPicker InternalCreateFileOpenPicker()
         {
             FileOpenPicker picker = new(_windowId);
@@ -37,12 +41,56 @@ namespace WsiuEngine.Core.System
         {
             return instance.InternalCreateFileSavePicker();
         }
-
         internal FileSavePicker InternalCreateFileSavePicker()
         {
             FileSavePicker picker = new(_windowId);
             InitializeWithWindow.Initialize(picker, _hwnd);
             return picker;
+        }
+
+        public static Task<PickFileResult> GetOpenFilePathAsync(params string[] fileTypeFilter)
+        {
+            return instance.InternalGetOpenFilePathAsync(fileTypeFilter);
+        }
+        internal Task<PickFileResult> InternalGetOpenFilePathAsync(params string[] fileTypeFilter)
+        {
+            FileOpenPicker picker = InternalCreateFileOpenPicker();
+            picker.SuggestedStartLocation = PickerLocationId.Desktop;
+            if (fileTypeFilter.Length == 0)
+            {
+                picker.FileTypeFilter.Add("*");
+            }
+            else
+            {
+                foreach (string filter in fileTypeFilter)
+                {
+                    picker.FileTypeFilter.Add(filter);
+                }
+            }
+            return picker.PickSingleFileAsync().AsTask();     
         }   
+
+        public static Task<PickFileResult> GetSaveFilePathAsync(params string[] fileTypeChoices)
+        {
+            return instance.InternalGetSaveFilePathAsync(fileTypeChoices);
+        }
+        internal Task<PickFileResult> InternalGetSaveFilePathAsync(params string[] fileTypeChoices)
+        {
+            FileSavePicker picker = InternalCreateFileSavePicker();
+            picker.SuggestedStartLocation = PickerLocationId.Desktop;
+            if (fileTypeChoices.Length == 0)
+            {
+                picker.FileTypeChoices.Add("any", ["*"]);
+            }
+            else if (fileTypeChoices.Length == 1)
+            {
+                picker.FileTypeChoices.Add(fileTypeChoices[0], ["*"]);
+            }
+            else
+            {
+                picker.FileTypeChoices.Add(fileTypeChoices[0], fileTypeChoices.Skip(1).ToList());
+            }
+            return picker.PickSaveFileAsync().AsTask(); 
+        }
     }
 }
