1a11d7fb04027054ebb719f3effca22473380068
power12562 <kimsiwoo1005@gmail.com>
2026-03-27 12:11:16 +0900

    feature(core): 엔진 루프 메인스레드에서 안전하게 처리하는 TaskDispatcher 추가

---
+++ WsiuEngine/Core/System/TaskDispatcher.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Concurrent;
+
+namespace WsiuEngine.Core.System
+{
+    internal class TaskDispatcher
+    {
+        private static TaskDispatcher instance = null!;
+        internal static void Initialize()
+        {
+            instance = new TaskDispatcher();    
+        }
+
+        private readonly ConcurrentQueue<Action> _taskQueue; 
+        private TaskDispatcher()
+        {
+            _taskQueue = [];
+        }
+
+        internal static void DispatchPendingTasks()
+        {
+            instance.InternalDispatchPendingTasks();
+        }
+        internal void InternalDispatchPendingTasks()
+        {
+            while (_taskQueue.TryDequeue(out Action? action) == true)
+            {
+                action();
+            }
+        }
+
+        internal static void PostToDispatcher(Action action)
+        {
+            instance.InternalPostToDispatcher(action);
+        }
+        internal void InternalPostToDispatcher(Action action)
+        {
+            _taskQueue.Enqueue(action);
+        }
+    }
+}
