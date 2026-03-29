3050c65e69510fab1ac6e9241169827040c1a89c
power12562 <kimsiwoo1005@gmail.com>
2026-03-29 20:01:15 +0900

    feature(editor): 로그 출력용 에디터 추가

---
+++ WsiuEditor/Editor/LogEditor.cs
@@ -0,0 +1,40 @@
+﻿using WsiuEditor.Editor.Base;
+using WsiuEditor.Interfaces;
+using WsiuEngine.Core;
+using WsiuEngine.Core.System;
+
+namespace WsiuEditor.Editor
+{
+    [SingletonEditor]
+    internal sealed partial class LogEditor : ImguiEditorBase
+    {
+        private const string editorName = "Log";
+        public LogEditor(Engine engine, ulong id) : base(engine, id)
+        {
+            _imguiContext.InitializeWindowClosable(editorName);
+            Name = editorName;
+            Log.OnLogReceived += OnLogReceived;
+        }
+
+        public override void Draw()
+        {
+
+        }
+
+        private void OnLogReceived(Log.Entry log)
+        {
+
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (IsDisposed)
+                return;
+            if (disposing)
+            {
+                Log.OnLogReceived -= OnLogReceived;
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
