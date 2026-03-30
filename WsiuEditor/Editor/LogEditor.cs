83f900522690aba22b4898f87f90006529bd508b
power12562 <kimsiwoo1005@gmail.com>
2026-03-30 23:12:34 +0900

    feature(core):로그 표시 에디터 기능 추가

--- WsiuEditor/Editor/LogEditor.cs
+++ WsiuEditor/Editor/LogEditor.cs
@@ -1,7 +1,11 @@
-﻿using WsiuEditor.Editor.Base;
+﻿using System;
+using System.Collections.Generic;
+using WsiuEditor.Editor.Base;
 using WsiuEditor.Interfaces;
 using WsiuEngine.Core;
 using WsiuEngine.Core.System;
+using WsiuEngine.Core.System.Keyboard;
+using WsiuRenderer;
 
 namespace WsiuEditor.Editor
 {
@@ -16,14 +20,32 @@ namespace WsiuEditor.Editor
             Log.OnLogReceived += OnLogReceived;
         }
 
+        private readonly List<string> _displayList = [];
+        private readonly List<string> _logfilePathList = [];
+        private readonly string _testLog = "";
         public override void Draw()
         {
-
+            static void Message(string message)
+            {
+                Log.Debug(message);
+            }
+            _imguiContext.InputText("Debug", _testLog, Message);
+            _imguiContext.BeginChild("LogConsoleRegion", new(0, 0), ImGuiChildFlags.Borders, ImGuiWindowFlags.HorizontalScrollbar);
+            void ClickItem(Int32 index)
+            {
+                if (InputSystem.GetKeyState(KeyCode.LeftControl) == KeyState.Held)
+                {
+                    Log.Debug($"경로: {_logfilePathList[index]}");
+                }
+            }
+            _imguiContext.DrawTextListClipper(_displayList, "어쩔툴팁", 2, ClickItem);
+            _imguiContext.EndChild();
         }
 
         private void OnLogReceived(Log.Entry log)
         {
-
+            _displayList.Add($"{Log.DisplayHeader(log)}\n{Log.DisplaySub(log)}");
+            _logfilePathList.Add(log.FilePath);
         }
 
         protected override void Dispose(bool disposing)
